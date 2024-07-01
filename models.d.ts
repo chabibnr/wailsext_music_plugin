ea13cc7ef515ca0a139003211363e067eab60500
Chabib Nurozak <chabibnurozak@gmail.com>
2024-07-01 18:21:32 +0700

    Init

---
+++ models.d.ts
@@ -0,0 +1,10 @@
+// models.d.ts
+// This file should contain any models that are used by the plugin.
+
+export namespace example_pluginPlugin {
+  export interface Hashes {
+    MD5: string;
+    SHA1: string;
+    SHA256: string;
+  }
+}
